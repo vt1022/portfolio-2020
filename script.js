8ce17125833161278327c394b933b90a3af43c7c
vt1022 <57807628+vt1022@users.noreply.github.com>
2020-05-14 16:53:39 -0400

    finished side nav.⛰

--- script.js
+++ script.js
@@ -1,7 +1,74 @@
-const vt = {}
+const vt = {};
 
+vt.TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 8) || 1000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
 
+vt.TxtRotate.prototype.tick = function() {
+  const i = this.loopNum % this.toRotate.length;
+  const fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  const that = this;
+  let delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+vt.textAnimation = function() {
+  const elements = document.getElementsByClassName('txt-rotate');
+  for (let i=0; i<elements.length; i++) {
+    const toRotate = elements[i].getAttribute('data-rotate');
+    const period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new vt.TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+}
+
+vt.sideNav = function() {
+  $('.nav-vertical__btn').on('click', function() {
+    $('html, body').animate({
+      scrollTop: $(`#${$(this).text()}`).offset().top - 100,
+    }, 400, 'linear');
+  })
+}
+
+vt.init = function() {
+  // remove focus on mouseleave
+  $('button, a, .container').on( "mouseleave", function() {
+    $(this).blur();
+  });
+  vt.textAnimation(); // code by https://codepen.io/gschier/pen/jkivt
+  vt.sideNav();
+}
 
 $(function() { 
-  
+  vt.init();
 });
